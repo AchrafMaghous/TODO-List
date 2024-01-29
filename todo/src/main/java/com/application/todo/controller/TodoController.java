db54b302083110d467cfa972bab86a81d6c223da
AchrafMaghous <muteallfocus7@gmail.com>
2024-01-29 15:11:54 +0100

    follow up soon

--- todo/src/main/java/com/application/todo/controller/TodoController.java
+++ todo/src/main/java/com/application/todo/controller/TodoController.java
@@ -1,5 +1,22 @@
 package com.application.todo.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TodoController {
-	
+	@GetMapping("/sngt")
+	@ResponseBody()
+	public ResponseEntity<Object> index() {
+		Map<String, String> data = new HashMap<>();
+		data.put("data", "hello sngt");
+
+		return new ResponseEntity<>(data, HttpStatus.OK);
+	}
 }
